2b47a5d3d5899309df5c4de8241fb9d294018a4a
Alfie Baxter <34726358+Colemakman@users.noreply.github.com>
2024-10-01 13:38:12 +0100

    Improved coverage

--- tests/test_present.py
+++ tests/test_present.py
@@ -8,6 +8,13 @@ def test_empty_present():
         p.unwrap()
     assert str(e.value) == "No contents have been wrapped."
 
+def test_already_unwrapped_present():
+    p = Present()
+    p.wrap("Bongo")
+    with pytest.raises(Exception) as e:
+        p.wrap("Flute")
+    assert str(e.value) == "A contents has already been wrapped."
+
 def test_default_present():
     p = Present()
     p.wrap("Guitar")
